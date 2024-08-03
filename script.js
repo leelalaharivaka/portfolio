2f3da8b6693f4dd6662579ffa9946027e7e4cea4
leelalaharivaka <144522929+leelalaharivaka@users.noreply.github.com>
2024-08-03 18:48:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed= new typed(".text",{
+    String: ["Frontend Developer", "YouTuber", "Web Developer"],
+    typeSpeed:100;
+    backspeed:100;
+    backDelay:1000;
+    loop:true
+    
+})
